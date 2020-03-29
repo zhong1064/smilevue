6d5077b75c15c4d993c1e9ecccaeb58d829ce3b3
zhongkun <1064598949@qq.com>
2020-03-29 22:41:20 +0800

    rem适配

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import { Button } from 'vant'
+import 'vant/lib/index.css';
 Vue.config.productionTip = false
-
+Vue.use(Button)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
